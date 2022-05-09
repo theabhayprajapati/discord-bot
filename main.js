9523a4a83a2628a49ae53f3ea7f4e364f28c148c
theabhayprajapati <theabhayprajapati@gmail.com>
2022-05-09 09:14:47 +0530

    added to Main.js

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 import Discord, { Client } from "discord.js";
 import dotenv from "dotenv";
-import fetch from "node-fetch";
 dotenv.config()
 // let bot ready wrtie the msgs
 const client = new Client({
@@ -20,25 +19,6 @@ client.on("ready", () => {
     });
 });
 
-const AiMageAPI = async () => {
-    let headersList = {
-        "Accept": "*/*",
-        "User-Agent": "Thunder Client (https://www.thunderclient.com)",
-        "Content-Type": "application/json"
-    }
-
-    let bodyContent = JSON.stringify({ "Id": "7502889418978", "tags": ["applique", "bag", "bead", "belt", "blouse", "bow", "cape", "coat", "collar", "dress", "epaulette", "flower", "glasses", "hair accessory", "head covering", "headband", "jacket", "lapel", "neckline", "pants", "pocket", "ribbon"], "shopName": "wethekoolive.myshopify.com" });
-
-    fetch("https://yrpaxzdyg6.execute-api.us-west-2.amazonaws.com/prod", {
-        method: "POST",
-        body: bodyContent,
-        headers: headersList
-    }).then(function (response) {
-        return response.text();
-    }).then(function (data) {
-        console.log(data);
-    })
-}
 // make simple hello bot
 client.on("messageCreate", async message => {
     console.log(message);
@@ -49,12 +29,6 @@ client.on("messageCreate", async message => {
         await message.channel.send("Hello");
     }
 
-    // if msg contain fetch 
-    if (message.content.toLowerCase().includes("fetch")) {
-        const data = await AiMageAPI()
-        await message.channel.send("Fetching...")
-        // await message.channel.send(data.body);
-    }
 
     // send dm to user
     if (message.content.toLowerCase().includes("dm")) {
