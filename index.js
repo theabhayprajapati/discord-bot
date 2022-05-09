0367a557f3af5293e3eb0d82362f0880fb6cf27b
theabhayprajapati <theabhayprajapati@gmail.com>
2022-05-09 10:22:31 +0530

    added push express.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world")
+import express from 'express';
+const app = express();
+
+const port = 3000;
+// app.use(express.json());
+app.use("/", (req, res)=>{
+    res.json({
+        message: "Hello World"
+    })
+})
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+})
