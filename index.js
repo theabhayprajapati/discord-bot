d3eba246e7ace660b2999a092f4dd075acd2ef25
theabhayprajapati <theabhayprajapati@gmail.com>
2022-05-09 10:36:39 +0530

    discord js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+import Discord from "discord.js";
 import express from 'express';
 const app = express();
-
+console.log("Discord.js version: " + Discord.version + "\n" + Discord);
 const port = 3000;
 // app.use(express.json());
-app.use("/", (req, res)=>{
+app.use("/", (req, res) => {
     res.json({
         message: "Hello World"
     })
