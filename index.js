78877219dbe51a48fd8a63e7950de3399de66f95
theabhayprajapati <theabhayprajapati@gmail.com>
2022-05-26 13:53:27 +0530

    removed express

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import Discord, { Client } from "discord.js";
 import dotenv from "dotenv";
-import express from 'express';
+// import express from 'express';
 import fetch from 'node-fetch';
 dotenv.config();
-const app = express();
+// const app = express();
 console.log("Discord.js version: " + Discord.version + "\n" + Discord);
 const port = 3000;
-
 
 const client = new Client({
     disableEveryone: true,
@@ -103,13 +102,4 @@ client.on('messageCreate', async (msg) => {
             }
             )
     }
-})
-// app.use(express.json());
-app.use("/", (req, res) => {
-    res.json({
-        message: "Hello World"
-    })
-})
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
 })
