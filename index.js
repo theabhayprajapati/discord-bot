5bd18788386560ba7b88523b79f107c8ee941a23
theabhayprajapati <theabhayprajapati@gmail.com>
2022-05-28 15:59:05 +0530

    add server

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 import Discord, { Client } from "discord.js";
 import dotenv from "dotenv";
 // import express from 'express';
+
 import fetch from 'node-fetch';
+// make a server with express
+import express from 'express';
+// import leetcode from './api/leetcode.js';
 dotenv.config();
+
+const app = express();
+// app.use('/leetcode', leetcode);
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.json({
+        this: "this is leetcode api",
+        leetcode: "https://leetcode.com/abhayprajapati",
+        github: "https://github.com/theabhayprajapati",
+        twitter: "https://twitter.com/abhayprajapati_"
+    })
+})
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
 // const app = express();
 console.log("Discord.js version: " + Discord.version + "\n" + Discord);
 const port = 3000;
